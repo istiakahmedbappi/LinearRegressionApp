9131747d65ee9d2c2199cdbbee51d795699bcec5
istiakahmedbappi <istiakbappi@gmail.com>
2026-08-23 23:11:59 +0600

    Fix typo in Mean Squared Error output

--- myapp.py
+++ myapp.py
@@ -54,5 +54,5 @@ y_pred=model.predict(X_test)
 mse=mean_squared_error(y_test, y_pred)
 r2=r2_score(y_test,y_pred)
 st.subheader("Model Evaluation")
-st.write(f"Mean Squread Error: {mse: .2f})
+st.write(f"Mean Squread Error: {mse: .2f}")
 st.write(f"R^2Score: Score: {r2:.2f}" 
