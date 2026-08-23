dde3eb0edc5fbcb4fda416eb636feae85592bf52
istiakahmedbappi <istiakbappi@gmail.com>
2026-08-23 23:37:50 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -70,4 +70,13 @@ for feature in features:
             input_data[feature] = float(user_val)
     except ValueError:
         valid_input = False
-
+if st.button("Predict"):
+  if valid_input:
+    input_df=pd.Dataframe([input_data])
+    input_scaled=scaler.transform(input_df)
+    prediction=model.predict(input_scaled)
+    st.success(f"Predicted {target}: {prediction[0]:.2f}")
+    
+  else:
+    st.error("please enter valid numeric values for all features before predecting.")
+    
