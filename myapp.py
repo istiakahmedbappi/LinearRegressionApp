c70539d25060eb1662c202db3a28ee9d17ceb8dd
istiakahmedbappi <istiakbappi@gmail.com>
2026-08-23 21:32:58 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 st.title("Linear Regression Web Application")
-
+st.subheader("Data dcience learning")
          
