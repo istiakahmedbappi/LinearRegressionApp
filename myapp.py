afc206ac98695ffbf533c33ed6d24bb24fff83af
istiakahmedbappi <istiakbappi@gmail.com>
2026-08-23 22:31:13 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -33,7 +33,7 @@ numeric_cols = df.select_dtypes(include=np.number).columns.tolist()
 if len(numeric_cols)<2:
   st.error("Need at least two numeric columns for regression.")
   st.stop()
-  
-
+ 
+st.selectbox("Select target variable", numeric_cols)
 
 
